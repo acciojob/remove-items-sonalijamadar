e78b6e67b8f526b4a5dd7130bf3a4501ddfb0d6b
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2025-12-31 15:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const button = document.querySelector('input[type="button"]');
+button.addEventListener("click" , () => {
+	const select = document.getElementById("colorSelect");
+	const selectedIndex = select.selectedIndex;
+
+	if(selectedIndex !== -1){
+		select.remove(selectedIndex);
+	};
+});
